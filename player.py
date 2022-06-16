6525b285f4f2981b5e1ae3dbc6714134bc171e84
Phamtaums2 <107648935+Phamtaums2@users.noreply.github.com>
2022-06-16 22:16:27 +0200

    test
    Test de modif

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ import pygame
 import time
 class Player(pygame.sprite.Sprite):
 
-   def __init__(self, x, y)
+   def __init__(self, x, y):
        super().__init__()
        self.sprite_sheet = pygame.image.load("player.png")
        self.image = self.get_image(0, 0)
