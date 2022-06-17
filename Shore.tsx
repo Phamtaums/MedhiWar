bdf75e66b7b71e6cc958ff957bc631f934c2eee2
Phamtaums <66764359+Phamtaums@users.noreply.github.com>
2022-06-17 17:34:25 +0200

    Delete Shore.tsx

--- Shore.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Shore" tilewidth="16" tileheight="16" tilecount="5" columns="5">
- <image source="Shore.png" width="80" height="16"/>
-</tileset>
