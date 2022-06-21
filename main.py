935102f61e32cdb337562e120ebdfb21f7826bcc
Phamtaums <66764359+Phamtaums@users.noreply.github.com>
2022-06-21 17:49:32 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pygame
-pygame.init()
-
-from game import Game
-
-if __name__ == '__main__':                # chargement de la base du module
-    pygame.init()                         # chargement des composants
-    game = Game()                         # instancié la classe game
-    game.run()                          # exécution du programme
